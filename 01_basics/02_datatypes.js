8cec0686267cd5fe954b5e184ef5995ba9937076
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-07-27 21:59:59 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3)  // code  readability should be high
+
+let name = "Waleed ANJUM"
+let age = 26
+let isLoggedIn = false
+let state ;
+
+// primitive data types 
+
+//number => 2 to power 53 
+// bigint  (large value like stock market etc )
+
+// string => ""
+
+// boolean => true/false
+//null => standalone value (empty means kvh hai he nae )
+
+//undefined => value assign he nae hoe hai 
+
+//symbol => unique 
+
+
+// Now 
+
+// objects 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //objects

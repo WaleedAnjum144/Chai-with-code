ef673dc9744132f6f1e07029e3c72812554dae8c
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-07-28 09:49:47 +0000

    ConversionOp

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,32 @@
+let  score = "Waleed"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number (score)
+
+//console.log(typeof valueInNumber)
+//console.log( valueInNumber)
+
+
+//"33" =>33
+// "33abc" => NaN
+//true => 1; false=>0
+
+
+let isLoggedIn = "Waleed"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+
+// "" =>false  
+//"waleed" => true
+
+let number = 22
+
+let stringNumber = String(number)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);

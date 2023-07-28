81804eae3c55426ef1119a17fc014feae7abea6b
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-07-28 10:52:19 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null>= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);      //avoid all above comparision 
+
+
+
+
+
+
+//=== 
+console.log("2" == 2);

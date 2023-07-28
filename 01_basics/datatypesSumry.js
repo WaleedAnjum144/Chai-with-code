025add41390b9557cdea6f0455313ae1a9c147c8
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-07-28 19:26:42 +0000

    DataTypes typesof Done

---
+++ 01_basics/datatypesSumry.js
@@ -0,0 +1,50 @@
+// primitive and Non-primitive 
+
+//#Primitive
+
+// 7 types: String , Number Boolean , null , undefined,symbol,Bigint
+
+
+
+const score = 100 
+
+const scoreValue =  100.3 
+
+const isLoggedIn = false 
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol ('123')
+
+const anotherId = Symbol('123')
+
+// console.log (id === anotherId)
+
+const bigNumber = 245452424254n
+
+
+
+
+
+
+// Reference (Non-Primitive)
+
+//Array , Objects, Functions
+//Array form
+const heros = ["Imam Hussain" , "Khalid Bin Waleed", "Khizar" ];
+
+// object Form 
+
+let myObj = {
+    name :"Waleed",
+    age :22,
+}
+
+// functions 
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log( typeof id)

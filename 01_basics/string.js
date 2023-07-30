b7a87bc096479f421f5a15973315c839706d4eb9
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-07-30 20:46:56 +0000

    String Done

---
+++ 01_basics/string.js
@@ -0,0 +1,16 @@
+const name = "Waleed"
+
+const age = 26
+
+// console.log (name + " age is " + age )
+
+// `` string interpolation 
+
+// console.log(`My name is ${name} and  age is ${age} years old`);
+
+const gameName = new String('Waleed Anjum')
+
+// console.log(gameName.indexOf('m'))
+
+const str = gameName.includes("Ali")
+console.log(str)

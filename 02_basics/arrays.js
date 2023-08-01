0179cd444b3051d27a0a8cbf68e5e897ae5b7e05
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-08-01 06:31:19 +0000

    Array 1

---
+++ 02_basics/arrays.js
@@ -0,0 +1,20 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5]
+// console.log(myArr)
+// console.log(myArr.length)
+// myArr[3] = 5
+
+
+// Array nethod  
+
+
+// myArr.push(6)
+// myArr.pop()
+
+
+console.log(myArr.indexOf(3))
+
+const newarr = myArr.join()
+console.log(myArr)
+console.log(typeof newarr)

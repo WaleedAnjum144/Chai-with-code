6be37d1daedf6610dd232de790ce6278bf8f88f3
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-08-02 07:58:17 +0000

    Scope Done

---
+++ 03_basics/global_local.js
@@ -0,0 +1,17 @@
+// Out side the  is called global scope is present in the 
+//block scop but block scop is not presen out side the block scope
+
+
+let c = 300
+
+if(true){
+  
+    const b =20       // under the curly braces the area is called block scope
+    let c = 3330
+    console.log(c)
+    
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);

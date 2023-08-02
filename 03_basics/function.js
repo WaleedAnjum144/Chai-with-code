8f382d5115ec50f111a8655414f91bd05284b1be
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-08-02 06:39:14 +0000

    Function with array and object

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -36,4 +36,40 @@ function loginUserMessage(username= "Sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("User2"))
+// console.log(loginUserMessage("User2"))
+
+
+function calculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,8000))
+
+const user ={
+    username : "Waleed Anjum",
+    price:99
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+
+handleObject({
+    username:"Waleed Ansari",
+    price:300
+})
+
+// array 
+
+
+const myNewArray = [200,300,400,500]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,300,400,500]))

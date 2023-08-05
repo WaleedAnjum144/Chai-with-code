56ffbc563a129f74d46ad3b29dff07a82c606df4
Waleed Anjum <98604837+Waleedweber@users.noreply.github.com>
2023-08-05 08:11:14 +0000

    Control flow done

---
+++ control_flow/one.js
@@ -0,0 +1,27 @@
+// if 
+
+const temperature = 42
+
+
+
+
+// if(temperature  <  56){
+
+// console.log("Less than 56")
+
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+// const score = 200 
+
+// if(score > 100 ){
+    let power = "fly"  //var is the global scop variable means it will also acces the outside the curly braces 
+    // but if we use let varibale than it will use in the block scope means under the curly braces 
+    //not acces the out side of the curly braces 
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user powewr: ${power}`)
+
+
---
+++ control_flow/truthy.js
@@ -0,0 +1,33 @@
+const userEmail = []
+
+// if (userEmail){
+//     console.log("got useremail");
+
+// } else {
+//     console.log("Dont have user email");
+// }
+
+// falsy values 
+
+// false its self false 
+// 0 ,-0 , BigInt 0n , "" , null , undefined , NaN
+
+// TrUTHT VALUES    
+
+// "0" , "false", " ", [],{}, function(){}
+
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length === 0){
+// console.log("Object is empty")
+// }
+
+//terniery Operator 
+
+const iceTeaPrice = 100 
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
